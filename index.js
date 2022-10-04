d06c019f2657adaadd5a4c7f3cf8358271078f11
sahil <sahildahiya423@gmail.com>
2022-10-04 15:05:51 +0530

    hi

---
+++ index.js
@@ -0,0 +1,12 @@
+        let a =20
+         b=10
+        let c=a+b
+        d=a*b
+        e=a/b
+        f=a-b
+        g=a-- + --a
+        console.log("addition is "+ c)
+        console.log("substraction is "+ f)
+        console.log("multiplication is "+ d)
+        console.log("division is "+ e)
+        console.log(g)
